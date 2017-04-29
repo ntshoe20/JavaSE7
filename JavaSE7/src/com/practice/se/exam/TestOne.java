23e646c99706e78ad82bf56fbd16665670aa4b71
Itu <Itu@DESKTOP-8R76HEK>
2017-04-29 19:34:00 +0200

    I was testing the Github and eclipse linkup and all the connections part one.. Regards Itu

---
+++ JavaSE7/src/com/practice/se/exam/TestOne.java
@@ -0,0 +1,12 @@
+package com.practice.se.exam;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I cant believe this github link");
+
+	}
+
+}
